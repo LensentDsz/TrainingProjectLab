3c6e83a4db993016e6a112359f6996b89254bb34
LensentDsz <dsz970117@gmail.com>
2020-11-30 19:12:38 +0800

    Class Bricks

---
+++ Program/src/Bricks.java
@@ -0,0 +1,43 @@
+package Supermario;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+
+public class Bricks extends Things{
+
+	
+	protected static BufferedImage Bricks = null;
+	protected int BricksWidth,BricksHeight;
+	{
+	try {
+		Bricks = ImageIO.read((ImageInputStream) new File("Img/Bricks.png"));
+	} catch (IOException e) {
+		System.out.println("Unable load Bricks!");
+	}
+	}
+	Bricks(Game game ,int x , int y , int BricksWidth ,int BricksHeight){
+		super(x,y,game);
+		this.BricksWidth=BricksWidth;
+		this.BricksHeight=BricksHeight;
+		TotalWidth=30*BricksWidth;
+		TotalHeight=30*BricksHeight;
+	}
+	public void Paint(Graphics g)
+	{
+		super.Paint(g);
+		Image img = null;
+		for(int i=0;i<BricksWidth;i++)
+		{
+			for(int j=0;j<BricksHeight;j++)
+			{
+				img = Bricks;
+				g.drawImage(img, x+(i-1)*UnitWidth,y+(j-1)*UnitHeight, null);
+			}
+		}
+		
+	}
+}
